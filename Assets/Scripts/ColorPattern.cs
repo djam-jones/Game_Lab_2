0396ff4eb052cba786aa68d00035173668e4d454
Kramboja <koen5067@icloud.com>
2015-05-18 14:50:57 +0200

    licht switch + storing

--- Assets/Scripts/ColorPattern.cs
+++ Assets/Scripts/ColorPattern.cs
@@ -4,28 +4,54 @@ using System.Collections.Generic;
 
 public class ColorPattern : MonoBehaviour {
 
-	private Color 		_red 		= new Color(1, 0, 0);
-	private Color 		_green 		= new Color(0, 1, 0);
-	private Color 		_blue 		= new Color(0, 0, 1);
-	private Color 		_yellow 		= new Color(1, 1, 0);
+	//private Color 		_red 		= new Color(1, 0, 0);
+	//private Color 		_green 		= new Color(0, 1, 0);
+	//private Color 		_blue 		= new Color(0, 0, 1);
+	//private Color 		_yellow 	= new Color(1, 1, 0);
+
+	public Color[] 		colors;//	= new Color[_red, _green, _blue, _yellow];
+
+	public int[]		_pastCollection;
 
 	private int 		_roundIndex;
+	private int			_currentRound;
 	private int[] 		_colorIndex;
-	private float		_invokeTime = 0.5f;
-	private float 		_repeatRate = 1f;
+	private float		_invokeTime = 1f;
+	private float 		_repeatRate = 0.8f;
 
 	public Light localLight;
 
 	void Awake()
 	{
+		if(_roundIndex == 0)
+		{
+			_roundIndex = 900;
+		}
+
 		localLight = GetComponent<Light>();
-		localLight.color = Color.white;
-		//InvokeRepeating("ChangeColor", _invokeTime, _repeatRate);
+		turnLightOff();
+		InvokeRepeating("ChangeColor", _invokeTime, _repeatRate);
 	}
 
 	void ChangeColor()
 	{
+		if(_currentRound == _roundIndex)
+		{
+			CancelInvoke("ChangeColor");
+			return;
+		}
+		int nextColor = Random.Range(0,4);
 
+		_pastCollection[_currentRound] = nextColor;
+
+		localLight.color = colors[nextColor];
+		Invoke("turnLightOff", 0.5f);
+
+		_currentRound++;
 	}
 
+	void turnLightOff()
+	{
+		localLight.color = Color.white;
+	}
 }
