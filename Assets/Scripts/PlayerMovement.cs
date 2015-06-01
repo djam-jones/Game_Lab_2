e0cdecf9649f32e7ab283366eed23356403e81b4
Djamali Shawn Jones <15546@ma-web.nl>
2015-06-01 12:22:42 +0200

    kfejkjfjkgk

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,27 +2,49 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
+	
+	private float _speed = 10;
+	private bool _onGround;
 
-	private float _speed = 10.0F;
-	private float _rotationSpeed = 100.0F;
+	private Vector3 _jumpVector = new Vector3(0, 7.5f, 0);
+	private Rigidbody _rigidbody;
 
-	private string _distance;
-	private int _shakes;
-
-	void awake()
+	void Awake()
 	{
-		InvokeRepeating("checkPosition",1,10);
+		_rigidbody = GetComponent<Rigidbody>();
 	}
 
-	void Update() 
+	void Update()
 	{
-		float translation = Input.GetAxis("Vertical") * _speed;
-		float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
-		
-		translation *= Time.deltaTime;
-		rotation *= Time.deltaTime;
-		
-		transform.Translate(0, 0, translation);
-		transform.Rotate(0, rotation, 0);
+		Movement();
+		print("Player On Ground is " + _onGround);
 	}
+
+	private void Movement()
+	{
+		float _x = Input.GetAxis("P1_H_L_Joystick") * _speed * Time.deltaTime;
+		float _z = Input.GetAxis("P1_V_L_Joystick") * _speed * Time.deltaTime;
+
+		//Moving
+		transform.Translate( new Vector3(_x, 0, _z) );
+
+		//Jump
+		if(Input.GetButtonDown("Cross") && _onGround)
+		{
+			_rigidbody.velocity = _jumpVector;
+		}
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		if(c.gameObject.tag == "Ground")
+		{
+			_onGround = true;
+		}
+		else if(c.gameObject.tag != "Ground")
+		{
+			_onGround = false;
+		}
+	}
+
 }
