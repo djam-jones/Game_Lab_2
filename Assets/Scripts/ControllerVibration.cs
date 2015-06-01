a7e200ed7e6eb985794b19431c38f6465d20f7d0
Kramboja <koen5067@icloud.com>
2015-06-01 12:02:48 +0200

    vibration test added

---
+++ Assets/Scripts/ControllerVibration.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerVibration : MonoBehaviour {
+
+	private string _distance;
+	private int _shakes;
+
+	void Awake()
+	{
+		InvokeRepeating("checkPosition",1,10);
+	}
+
+	private void checkPosition()
+	{
+		Debug.Log(_shakes);
+
+		if(_distance == "close")
+			_shakes = 3;
+		else if(_distance == "medium")
+			_shakes = 2;
+		else if(_distance == "far")
+			_shakes = 1;
+		else
+			return;
+		
+		InvokeRepeating("shake",1,1);
+	}
+	
+	private void shake()
+	{
+		Handheld.Vibrate();
+		_shakes -= 1;
+		if(_shakes == 0)
+			CancelInvoke("shake");
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.name == "close")
+			_distance = "close";
+		else if(other.gameObject.name == "medium")
+			_distance = "medium";
+		else
+			_distance = "far";
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.name == "close")
+			_distance = "Medium";
+		else if(other.gameObject.name == "medium")
+			_distance = "far";
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private float _speed = 10.0F;
+	private float _rotationSpeed = 100.0F;
+
+	private string _distance;
+	private int _shakes;
+
+	void awake()
+	{
+		InvokeRepeating("checkPosition",1,10);
+	}
+
+	void Update() 
+	{
+		float translation = Input.GetAxis("Vertical") * _speed;
+		float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
+		
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		
+		transform.Translate(0, 0, translation);
+		transform.Rotate(0, rotation, 0);
+	}
+}
